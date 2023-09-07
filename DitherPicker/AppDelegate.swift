dde3b080119672405ad076900510ca07b74dece9
FrostBird347 <39435218+FrostBird347@users.noreply.github.com>
2023-09-07 20:57:46 +0800

    Initial Commit

---
+++ DitherPicker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DitherPicker
+//
+//  Created by FrostBird on 7/9/23.
+//  Copyright © 2023 FrostBird347. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ DitherPicker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DitherPicker
+//
+//  Created by FrostBird on 7/9/23.
+//  Copyright © 2023 FrostBird347. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
